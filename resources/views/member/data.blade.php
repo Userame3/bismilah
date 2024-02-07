c716059fe9cd135379c775238ded686a8f965264
Userame3 <neoaffan02@gmail.com>
2024-02-07 14:35:04 +0700

    modified:   resources/views/member/data.blade.php  	modified:   resources/views/member/form.blade.php  	modified:   resources/views/member/index.blade.php  	modified:   resources/views/menu/data.blade.php  	modified:   resources/views/menu/form.blade.php  	modified:   resources/views/menu/index.blade.php

--- resources/views/member/data.blade.php
+++ resources/views/member/data.blade.php
@@ -1,12 +1,15 @@
 <!-- component -->
-    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table id="datatable" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+        <caption
+            class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
             Our products
-            <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p>
+            <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products
+                designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and
+                more.</p>
         </caption>
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
@@ -23,44 +26,54 @@
                     Alamat
                 </th>
                 <th scope="col" class="px-6 py-3">
-                  Action
+                    Action
                 </th>
             </tr>
         </thead>
         <tbody>
-          @foreach ($member as $p)
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{$i = (isset($i)?++$i:$i=1) }}
-                </th>
-                <td class="px-6 py-4">
-                    {{$p -> nama}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$p -> point}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$p -> alamat}}
-                </td>
-                <td class="px-6 py-4 ">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
-                </td>
-            </tr>
-          @endforeach
+            @foreach ($member as $p)
+                <tr class="bg-white dark:bg-gray-800">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $i = isset($i) ? ++$i : ($i = 1) }}
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $p->nama }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $p->point }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $p->alamat }}
+                    </td>
+                    <td class="px-6 py-4 ">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        <a href="#"
+                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
 
-<script>
-  let data = <?= $member ?>;
-  $(document).ready( ()=>{
-    $('#datatable').DataTable({
-      data:data,
-      columns:[
-        {data:'id',title:"awhfia"},
-        {data:'nama',title:"ANJING"},
-        {data:'action',title:"AN ahhhh"}
-      ]
-    });
-  })
+{{-- <script>
+    let data = ;
+    $(document).ready(() => {
+        $('#datatable').DataTable({
+            data: data,
+            columns: [{
+                    data: 'id',
+                    title: "awhfia"
+                },
+                {
+                    data: 'nama',
+                    title: "ANJING"
+                },
+                {
+                    data: 'action',
+                    title: "AN ahhhh"
+                }
+            ]
+        });
+    })
+</script> --}}
--- resources/views/menu/data.blade.php
+++ resources/views/menu/data.blade.php
@@ -1,13 +1,10 @@
 <!-- component -->
-    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table id="datatable" class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-            Our products
-            <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p>
-        </caption>
+
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
                 <th scope="col" class="px-6 py-3">
@@ -22,52 +19,61 @@
                 <th scope="col" class="px-6 py-3">
                     Deskripsi
                 </th>
-                 <th scope="col" class="px-6 py-3">
+                <th scope="col" class="px-6 py-3">
                     Harga
                 </th>
                 <th scope="col" class="px-6 py-3">
-                  Action
+                    Action
                 </th>
             </tr>
         </thead>
         <tbody>
-          @foreach ($menu as $p)
-            <tr class="bg-white dark:bg-gray-800">
-                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{$i = (isset($i)?++$i:$i=1) }}
-                </th>
-                <td class="px-6 py-4">
-                    {{$p -> nama_menu}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$p -> kategori}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$p -> deskripsi}}
-                </td>
-                <td class="px-6 py-4">
-                    {{$p -> harga}}
-                </td>
-                <td class="px-6 py-4 ">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
-                </td>
-            </tr>
-          @endforeach
+            @foreach ($menu as $p)
+                <tr class="bg-white dark:bg-gray-800">
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        {{ $i = isset($i) ? ++$i : ($i = 1) }}
+                    </th>
+                    <td class="px-6 py-4">
+                        {{ $p->nama_menu }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $p->kategori }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $p->deskripsi }}
+                    </td>
+                    <td class="px-6 py-4">
+                        {{ $p->harga }}
+                    </td>
+                    <td class="px-6 py-4 ">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        <a href="#"
+                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 </div>
 
 <script>
-  let data = <?= $menu ?>;
-  $(document).ready( ()=>{
-    $('#datatable').DataTable({
-      data:data,
-      columns:[
-        {data:'id',title:"No"},
-        {data:'nama',title:"Nama"},
-        {data:'action',title:"AN ahhhh"}
-      ]
-    });
-  })
+    let data = <?= $menu ?>;
+    $(document).ready(() => {
+        $('#datatable').DataTable({
+            data: data,
+            columns: [{
+                    data: 'id',
+                    title: "No"
+                },
+                {
+                    data: 'nama',
+                    title: "Nama"
+                },
+                {
+                    data: 'action',
+                    title: "AN ahhhh"
+                }
+            ]
+        });
+    })
 </script>
